091bea86c8b4752cb777914f8c83768786ff3dd5
Nhat Minh Luu <54903938+nluu175@users.noreply.github.com>
2024-03-13 01:38:41 -0600

    Add more source

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
 # https://realpython.com/python-web-scraping-practical-introduction/
+# https://medium.com/@chatur.agus.priyono/a-comprehensive-guide-to-scraping-data-from-linkedin-with-python-e128c46c5c74
+
+import re
 from urllib.request import urlopen
 
-
-url = "http://olympus.realpython.org/profiles/aphrodite"
-
-
+url = "http://olympus.realpython.org/profiles/dionysus"
 page = urlopen(url)
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
+html = page.read().decode("utf-8")
 
-
-print(html)
-
-
-title_index = html.find("<title>")
-start_index = title_index + len("<title>")
-
-print(start_index)
-
-end_index = html.find("</title>")
-
-print(end_index)
-
-title = html[start_index:end_index]
+pattern = "<title.*?>.*?</title.*?>"
+match_results = re.search(pattern, html, re.IGNORECASE)
+title = match_results.group()
+title = re.sub("<.*?>", "", title)  # Remove HTML tags
 
 print(title)
