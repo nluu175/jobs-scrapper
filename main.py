35870d7bdaa92de31538013f03935ddf50e19dec
Nhat Minh Luu <54903938+nluu175@users.noreply.github.com>
2024-03-17 23:28:56 -0600

    Add some scrapings

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# https://realpython.com/python-web-scraping-practical-introduction/
-# https://medium.com/@chatur.agus.priyono/a-comprehensive-guide-to-scraping-data-from-linkedin-with-python-e128c46c5c74
+import os
+from dotenv import load_dotenv
 
-import re
-from urllib.request import urlopen
+from linkedin_scraper import JobSearch, actions, Person
+from selenium import webdriver
 
-url = "http://olympus.realpython.org/profiles/dionysus"
-page = urlopen(url)
-html = page.read().decode("utf-8")
 
-pattern = "<title.*?>.*?</title.*?>"
-match_results = re.search(pattern, html, re.IGNORECASE)
-title = match_results.group()
-title = re.sub("<.*?>", "", title)  # Remove HTML tags
+load_dotenv()
 
-print(title)
+linkedin_email = os.getenv("LINKEDIN_EMAIL")
+linkedin_password = os.getenv("LINKEDIN_PASSWORD")
+
+
+driver = webdriver.Chrome()
+actions.login(driver, linkedin_email, linkedin_password)
+
+
+# job_search = JobSearch(driver=driver, close_on_complete=False, scrape=False)
+# # job_search contains jobs from your logged in front page:
+# # - job_search.recommended_jobs
+# # - job_search.still_hiring
+# # - job_search.more_jobs
+
+# job_listings = job_search.search(
+#     "Software Developer"
+# )  # returns the list of `Job` from the first page
+
+
+# print(job_listings)
+person = Person("https://www.linkedin.com/in/ln-minh/", driver=driver)
